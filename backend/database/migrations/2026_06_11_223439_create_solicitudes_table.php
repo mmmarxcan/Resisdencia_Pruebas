3d0eed622cee93c95b912cd4730c649f68cf75ae
Brayan <mmmarxcan@gmail.com>
2026-06-12 12:26:09 -0600

    Creacion de backedn y frontend

---
+++ backend/database/migrations/2026_06_11_223439_create_solicitudes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('solicitudes', function (Blueprint $table) {
+            $table->id();
+            $table->string('incoterm');
+            $table->string('tipo_servicio');
+            $table->text('descripcion_material');
+            $table->decimal('valor_factura',15,2);
+            $table->string('divisa', 3);
+            $table->decimal('peso_total', 10,2);
+            $table->string('unidad_peso', 2);
+            $table->integer('numero_bultos');
+            $table->boolean('mercancia_adicionales')->nullable(false);
+            $table->json('servicios_adicionales')->nullable();
+            $table->string('estatus')->default('pendiente');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('solicitudes');
+    }
+};
