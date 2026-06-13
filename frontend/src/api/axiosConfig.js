ba3043653dcf56211dfe508fe402744d655951e0
Brayan <mmmarxcan@gmail.com>
2026-06-13 16:19:55 -0600

    corregir errores

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -4,7 +4,7 @@ const api = axios.create({
     baseURL: 'http://127.0.0.1:8000/api',
     headers: {
         'Content-Type': 'application/json',
-        'Accept': 'ápplication/json' 
+        'Accept': 'application/json' 
     },
 });
 
--- frontend/src/components/SolicitudForm.jsx
+++ frontend/src/components/SolicitudForm.jsx
@@ -1,29 +1,31 @@
-import  { useState} from 'react';
-import api from '../api/axiosConfig';
+import { useState } from "react";
+import api from "../api/axiosConfig";
 
 const FormularioSlicitud = () => {
-    const [formData, setFormData] = useState({
-        // sacamos los datos de nuestro requesto que esta en la ruta app/http/requests
+  const [formData, setFormData] = useState({
+    // sacamos los datos de nuestro requesto que esta en la ruta app/http/requests
 
-        incoterm: '',
-        tipo_servicio: '',
-        descripcion_material: '',
-        valor_factura: '',
-        divisa: '',
-        peso_total: '',
-        unidad_peso: '',
-        numero_bultos: '',
-        mercancia_adicionales: false,
-        
-        servicos_adicionales: {
-            seguro: false,
-            maniobra_descarga: false
-        },
-        direccion: [{
-            tipo: 'origen',
-            direccion_origen: '',
-            direccion_destino: ''
-        }
-        ]
-        });
-}
+    incoterm: "",
+    tipo_servicio: "",
+    descripcion_material: "",
+    valor_factura: "",
+    divisa: "",
+    peso_total: "",
+    unidad_peso: "",
+    numero_bultos: "",
+    mercancia_adicionales: false,
+
+    servicios_adicionales: {
+      seguro: false,
+      maniobra_descarga: false,
+    },
+    direccion: [
+      {
+        tipo: "origen",
+        direccion_origen: "",
+        direccion_destino: "",
+      },
+    ],
+  });
+  
+};
