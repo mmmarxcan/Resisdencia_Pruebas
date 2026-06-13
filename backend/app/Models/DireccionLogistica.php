93f1c0c5ebb7a142d08f2bff5d83543adc58d781
Brayan <mmmarxcan@gmail.com>
2026-06-12 18:36:25 -0600

    Modelo de direccion logistica finalizado

--- backend/app/Models/DireccionLogistica.php
+++ backend/app/Models/DireccionLogistica.php
@@ -2,9 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class DireccionLogistica extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'direcciones_logisticas';
+
+    protected $fillable = [
+        'solicitud_id',
+        'tipo',
+        'direccion',
+        'direcciones_logisticas',
+    ];
+
+    public function solicitud(){
+        return $this->belongsTo(Solicitud::class, 'solicitud_id');
+    }
 }
